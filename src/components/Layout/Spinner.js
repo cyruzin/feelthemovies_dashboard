e16b579057c7f18ffc9d3bc6afeecd5cb526cb30
Cyro Dubeux <xorycx@gmail.com>
2018-10-26 20:33:57 -0300

    Spinner Component

---
+++ src/components/Layout/Spinner.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Spinner from '../../assets/img/Loading.gif'
+
+export default () => (
+    <div>
+        <section className="no-padding-top">
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-lg-12 text-center">
+                        <img src={Spinner} alt="Loading" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+)
