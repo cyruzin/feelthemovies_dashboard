d93de1f11267d5129b8139fe2b83cc0be008e732
Cyro Dubeux <xorycx@gmail.com>
2019-02-06 19:29:26 -0300

    constants fix

--- src/util/constants/index.js
+++ src/util/constants/index.js
@@ -2,11 +2,10 @@ export const tmdbToken = '9aca69849a23528a419aea463387945f'
 export const tinyMCEKey = '524aoctgpx14f8bvkwp4nwtstg3qzosyouqmz0dkqto0mv11'
 
 // dev
-// export const baseURL = 'http://api.feelthemovies.local/v1'
-// export const authURL = 'http://api.feelthemovies.local/auth'
+//export const baseURL = 'http://localhost:8000/v1'
+//export const authURL = 'http://localhost:8000/v1/auth'
+
+// production 
 export const baseURL = 'http://157.230.141.254:8000/v1'
 export const authURL = 'http://157.230.141.254:8000/v1/auth'
 
-// production 
-// export const baseURL = 'https://api.feelthemovies.com.br/v1'
-// export const authURL = 'https://api.feelthemovies.com.br/auth'
