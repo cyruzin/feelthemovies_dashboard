d9419a553e98276983dc0fd6a593b80e811c70a1
Cyro Dubeux <xorycx@gmail.com>
2019-02-16 22:22:47 -0300

    It's FOSS

--- src/util/constants/index.js
+++ src/util/constants/index.js
@@ -1,11 +1,8 @@
-export const tmdbToken = '9aca69849a23528a419aea463387945f'
-export const tinyMCEKey = '524aoctgpx14f8bvkwp4nwtstg3qzosyouqmz0dkqto0mv11'
+// TODO: Send this info from the back-end
+export const tmdbToken = 'YOUR_TMDB_TOKEN'
 
 // dev
-//export const baseURL = 'http://localhost:8000/v1'
-//export const authURL = 'http://localhost:8000/v1/auth'
+export const baseURL = 'http://localhost:8000/v1'
+export const authURL = 'http://localhost:8000/v1/auth'
 
-// production 
-export const baseURL = 'http://157.230.141.254:8000/v1'
-export const authURL = 'http://157.230.141.254:8000/v1/auth'
 
