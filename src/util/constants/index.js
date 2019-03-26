d4762f57861e33c57d8b1ed02a4fd0edfaa3090f
Cyro Dubeux <xorycx@gmail.com>
2019-03-25 21:14:05 -0300

    Path change

--- src/util/constants/index.js
+++ src/util/constants/index.js
@@ -3,6 +3,6 @@ export const tmdbToken = 'YOUR_TMDB_TOKEN'
 
 // dev
 export const baseURL = 'http://localhost:8000/v1'
-export const authURL = 'http://localhost:8000/v1/auth'
+export const authURL = 'http://localhost:8000/auth'
 
 
