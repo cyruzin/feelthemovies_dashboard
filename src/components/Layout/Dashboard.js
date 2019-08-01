054fcdeb864093cc507bb2df7b5b1c98e5bd2347
Cyro Dubeux <cyro.dubeux@servicenet.com.br>
2019-08-01 14:00:05 -0300

    Edit Recommendation Route / Screen

--- src/components/Layout/Dashboard.js
+++ src/components/Layout/Dashboard.js
@@ -2,44 +2,39 @@ import React, { useEffect } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { loadJs } from '../../util/helpers'
 
-/** Route Components. */
+/** Layout Components. */
 import Header from './Header'
 import Content from './Content'
 
+/** Route Components. */
 import Recommendations from '../Recommendations/Recommendations'
 import RecommendationsCreate from '../Recommendations/RecommendationsCreate'
-//import RecommendationsEdit from '../Recommendations/RecommendationsEdit'
-//import RecommendationsDelete from '../Recommendations/RecommendationsDelete'
+import RecommendationsEdit from '../Recommendations/RecommendationsEdit'
 import RecommendationsSearch from '../Recommendations/RecommendationsSearch'
 
-// import Sources from '../Sources/Sources'
-// import SourcesCreate from '../Sources/SourcesCreate'
-// import SourcesEdit from '../Sources/SourcesEdit'
-// import SourcesDelete from '../Sources/SourcesDelete'
-// import SourcesSearch from '../Sources/SourcesSearch'
+// import RecommendationItems from '../RecommendationItems/RecommendationItems'
+// import RecommendationItemsCreate from '../RecommendationItems/RecommendationItemsCreate'
+// import RecommendationItemsEdit from '../RecommendationItems/RecommendationItemsEdit'
 
 // import Users from '../Users/Users'
 // import UserRegistration from '../Users/UserRegistration';
 // import UserEdit from '../Users/UserEdit';
-// import UserDelete from '../Users/UserDelete';
 // import UserSearch from '../Users/UserSearch'
 
 // import Genres from '../Genres/Genres'
 // import GenresCreate from '../Genres/GenresCreate'
 // import GenresEdit from '../Genres/GenresEdit'
-// import GenresDelete from '../Genres/GenresDelete'
 // import GenresSearch from '../Genres/GenresSearch'
 
 // import Keywords from '../Keywords/Keywords'
 // import KeywordsCreate from '../Keywords/KeywordsCreate'
 // import KeywordsEdit from '../Keywords/KeywordsEdit'
-// import KeywordsDelete from '../Keywords/KeywordsDelete'
 // import KeywordsSearch from '../Keywords/KeywordsSearch'
 
-// import RecommendationItems from '../RecommendationItems/RecommendationItems'
-// import RecommendationItemsCreate from '../RecommendationItems/RecommendationItemsCreate';
-// import RecommendationItemsEdit from '../RecommendationItems/RecommendationItemsEdit';
-// import RecommendationItemsDelete from '../RecommendationItems/RecommendationItemsDelete';
+// import Sources from '../Sources/Sources'
+// import SourcesCreate from '../Sources/SourcesCreate'
+// import SourcesEdit from '../Sources/SourcesEdit'
+// import SourcesSearch from '../Sources/SourcesSearch'
 
 import Logout from '../Authentication/Logout'
 
@@ -53,39 +48,33 @@ function Dashboard () {
             <Header />
             <Content>
                 <Switch>
-                    {/* <Route path='/dashboard/users' component={Users} />
-                    <Route path='/dashboard/create_user' component={UserRegistration} />
-                    <Route path='/dashboard/edit_user/:id' component={UserEdit} />
-                    <Route path='/dashboard/delete_user/:id' component={UserDelete} />
-                    <Route path='/dashboard/search_user' component={UserSearch} /> */}
-
                     <Route path='/dashboard/recommendations' component={Recommendations} />
                     <Route path='/dashboard/create_recommendation' component={RecommendationsCreate} />
-                    {/*<Route path='/dashboard/edit_recommendation/:id' component={RecommendationsEdit} />
-                    <Route path='/dashboard/delete_recommendation/:id' component={RecommendationsDelete} /> */}
+                    <Route path='/dashboard/edit_recommendation/:id' component={RecommendationsEdit} />
                     <Route path='/dashboard/search_recommendation' component={RecommendationsSearch} />
 
                     {/* <Route path='/dashboard/items/:id' component={RecommendationItems} />
                     <Route path='/dashboard/create_item/:id' component={RecommendationItemsCreate} />
                     <Route path='/dashboard/edit_item/:id' component={RecommendationItemsEdit} />
-                    <Route path='/dashboard/delete_item/:id' component={RecommendationItemsDelete} />
+
+                    {/* <Route path='/dashboard/users' component={Users} />
+                    <Route path='/dashboard/create_user' component={UserRegistration} />
+                    <Route path='/dashboard/edit_user/:id' component={UserEdit} />
+                    <Route path='/dashboard/search_user' component={UserSearch} /> 
 
                     <Route path='/dashboard/genres' component={Genres} />
                     <Route path='/dashboard/create_genre' component={GenresCreate} />
                     <Route path='/dashboard/edit_genre/:id' component={GenresEdit} />
-                    <Route path='/dashboard/delete_genre/:id' component={GenresDelete} />
                     <Route path='/dashboard/search_genre' component={GenresSearch} />
 
                     <Route path='/dashboard/keywords' component={Keywords} />
                     <Route path='/dashboard/create_keyword' component={KeywordsCreate} />
                     <Route path='/dashboard/edit_keyword/:id' component={KeywordsEdit} />
-                    <Route path='/dashboard/delete_keyword/:id' component={KeywordsDelete} />
                     <Route path='/dashboard/search_keyword' component={KeywordsSearch} />
 
                     <Route path='/dashboard/sources' component={Sources} />
                     <Route path='/dashboard/create_source' component={SourcesCreate} />
                     <Route path='/dashboard/edit_source/:id' component={SourcesEdit} />
-                    <Route path='/dashboard/delete_source/:id' component={SourcesDelete} />
                     <Route path='/dashboard/search_source' component={SourcesSearch} /> */}
 
                     <Route path='/dashboard/logout' component={Logout} />
---
+++ src/components/Recommendations/RecommendationsEdit.js
@@ -0,0 +1,114 @@
+import React from 'react'
+import AntSelect from 'antd/lib/select'
+import AntSpin from 'antd/lib/spin'
+import 'antd/lib/select/style/css'
+import 'antd/lib/spin/style/css'
+import {
+    Section, SectionTitle, BreadCrumbs, FormGroup,
+    Input, TextArea, Select, Option, Button
+} from '../Common'
+
+const AntOption = AntSelect.Option
+
+function RecommendationsEdit () {
+    return (
+        <>
+            <BreadCrumbs
+                activeName="Edit"
+                breadCrumbs={[{
+                    key: 1,
+                    path: '/dashboard/recommendations',
+                    name: 'Recommendations'
+                }]} />
+            <Section>
+                <SectionTitle title="Edit Recommendation" />
+
+                <FormGroup label="Title">
+                    <Input className="form-control" />
+                </FormGroup>
+
+                <FormGroup label="Body">
+                    <TextArea className="form-control" />
+                </FormGroup>
+
+                <FormGroup label="Type">
+                    <Select className="form-control mb-3" >
+                        <Option value="0" defaultValue>Movie</Option>
+                        <Option value="1">TV Show</Option>
+                        <Option value="2">Mixed</Option>
+                    </Select>
+                </FormGroup>
+
+                <FormGroup label="Image">
+                    <AntSelect
+                        showSearch
+                        size='large'
+                        value={''}
+                        style={{ width: '100%' }}
+                        defaultActiveFirstOption={false}
+                        notFoundContent={
+                            true ? <AntSpin size="small" /> : null
+                        }
+                        showArrow={false}
+                        filterOption={false}
+                    // onSearch={this.fetchRecommendationImages}
+                    //onChange={this.handleRecommendationImage}
+                    >
+                        <AntOption key={1} value="test">
+                            Test
+                        </AntOption>
+                    </AntSelect>
+                </FormGroup>
+
+                <FormGroup label="Genres">
+                    <AntSelect
+                        mode="multiple"
+                        labelInValue
+                        size='large'
+                        //value={''}
+                        style={{ width: '100%' }}
+                        notFoundContent={
+                            true ? <AntSpin size="small" /> : null
+                        }
+                        showArrow={false}
+                        filterOption={false}
+                    // onSearch={this.fetchRecommendationImages}
+                    //onChange={this.handleRecommendationImage}
+                    >
+                        <AntOption key={1} value="test">
+                            Test
+                        </AntOption>
+                    </AntSelect>
+                </FormGroup>
+
+                <FormGroup label="Keywords">
+                    <AntSelect
+                        mode="multiple"
+                        labelInValue
+                        size='large'
+                        // value={''}
+                        style={{ width: '100%' }}
+                        notFoundContent={
+                            true ? <AntSpin size="small" /> : null
+                        }
+                        showArrow={false}
+                        filterOption={false}
+                    // onSearch={this.fetchRecommendationImages}
+                    //onChange={this.handleRecommendationImage}
+                    >
+                        <AntOption key={1} value="test">
+                            Test
+                        </AntOption>
+                    </AntSelect>
+                </FormGroup>
+
+                <FormGroup>
+                    <Button>Edit</Button>
+                </FormGroup>
+
+            </Section>
+        </>
+    )
+}
+
+export default RecommendationsEdit
