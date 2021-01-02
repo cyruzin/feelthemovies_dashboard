9e882f34ccd2184ecec23d1f8614b4855b50a6b5
Cyro Dubeux <xorycx@gmail.com>
2021-01-02 15:40:16 -0300

    useReactRouter lib removed

--- src/components/Common/SearchInput.js
+++ src/components/Common/SearchInput.js
@@ -1,53 +1,51 @@
 // @flow
-import React, { useState } from 'react'
-import useReactRouter from 'use-react-router'
-import InputLabel from './InputLabel'
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import InputLabel from "./InputLabel";
 
 type Props = {
-    path: string,
-    placeholder?: string
-}
+  path: string,
+  placeholder?: string,
+};
 
 SearchInput.defaultProps = {
-    placeholder: 'Busca'
+  placeholder: "Busca",
+};
+
+function SearchInput(props: Props) {
+  const { placeholder, path } = props;
+  const { push } = useHistory();
+  const [searchKeyword, setSearchKeyword] = useState("");
+
+  function searchHandler() {
+    if (searchKeyword === "") return false;
+
+    return push(`${path}?query=${searchKeyword}`, {
+      query: encodeURIComponent(searchKeyword),
+    });
+  }
+
+  function getKeyword(event) {
+    setSearchKeyword(event.target.value);
+  }
+
+  function isEnterPressed(event) {
+    if (event.keyCode === 13) searchHandler();
+  }
+
+  return (
+    <div className="form-group row">
+      <div className="col-lg-6">
+        <InputLabel
+          type="text"
+          className="form-control"
+          placeholder={placeholder}
+          onKeyDown={isEnterPressed}
+          onChange={getKeyword}
+        />
+      </div>
+    </div>
+  );
 }
 
-function SearchInput (props: Props) {
-    const { placeholder, path } = props
-    const { history } = useReactRouter()
-    const [searchKeyword, setSearchKeyword] = useState('')
-
-    function searchHandler () {
-        if (searchKeyword === '') return false
-
-        const { push } = history
-
-        return push(
-            `${path}?query=${searchKeyword}`,
-            { query: encodeURIComponent(searchKeyword) }
-        )
-    }
-
-    function getKeyword (event) {
-        setSearchKeyword(event.target.value)
-    }
-
-    function isEnterPressed (event) {
-        if (event.keyCode === 13) searchHandler()
-    }
-
-    return (
-        <div className="form-group row">
-            <div className="col-lg-6">
-                <InputLabel
-                    type="text"
-                    className="form-control"
-                    placeholder={placeholder}
-                    onKeyDown={isEnterPressed}
-                    onChange={getKeyword} />
-            </div>
-        </div>
-    )
-}
-
-export default SearchInput
+export default SearchInput;
--- src/components/Layout/Sidebar.js
+++ src/components/Layout/Sidebar.js
@@ -1,45 +1,57 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
-import useReactRouter from 'use-react-router'
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link, useLocation } from "react-router-dom";
 
-function Siderbar () {
-    const { location } = useReactRouter()
-    const user = useSelector(state => state.authentication.user)
+function Siderbar() {
+  const { pathname } = useLocation();
+  const user = useSelector((state) => state.authentication.user);
 
-    function activeCheck (pathname) {
-        if (pathname === location.pathname) return 'active'
-        return ''
-    }
+  function activeCheck(currentPath) {
+    if (currentPath === pathname) return "active";
+    return "";
+  }
 
-    return (
-        <nav id="sidebar">
-            <div className="sidebar-header d-flex align-items-center">
-                <div className="title">
-                    <h1 className="h5">Admin</h1>
-                    <p>{user.name}</p>
-                </div>
-            </div>
-            <span className="heading">Dash</span>
-            <ul className="list-unstyled">
-                <li className={activeCheck('/dashboard/users')}>
-                    <Link to='/dashboard/users'><i className="fa fa-user"></i>Users</Link>
-                </li>
-                <li className={activeCheck('/dashboard/recommendations')}>
-                    <Link to='/dashboard/recommendations'><i className="fa fa-thumbs-up"></i>Recommendations</Link>
-                </li>
-                <li className={activeCheck('/dashboard/genres')}>
-                    <Link to='/dashboard/genres'> <i className="fa fa-tags"></i>Genres </Link>
-                </li>
-                <li className={activeCheck('/dashboard/keywords')}>
-                    <Link to='/dashboard/keywords'> <i className="fa fa-hashtag"></i>Keywords </Link>
-                </li>
-                <li className={activeCheck('/dashboard/sources')}>
-                    <Link to='/dashboard/sources'> <i className="fa fa-play-circle"></i>Sources </Link>
-                </li>
-            </ul>
-        </nav>
-    )
+  return (
+    <nav id="sidebar">
+      <div className="sidebar-header d-flex align-items-center">
+        <div className="title">
+          <h1 className="h5">Admin</h1>
+          <p>{user.name}</p>
+        </div>
+      </div>
+      <span className="heading">Dash</span>
+      <ul className="list-unstyled">
+        <li className={activeCheck("/dashboard/users")}>
+          <Link to="/dashboard/users">
+            <i className="fa fa-user"></i>Users
+          </Link>
+        </li>
+        <li className={activeCheck("/dashboard/recommendations")}>
+          <Link to="/dashboard/recommendations">
+            <i className="fa fa-thumbs-up"></i>Recommendations
+          </Link>
+        </li>
+        <li className={activeCheck("/dashboard/genres")}>
+          <Link to="/dashboard/genres">
+            {" "}
+            <i className="fa fa-tags"></i>Genres{" "}
+          </Link>
+        </li>
+        <li className={activeCheck("/dashboard/keywords")}>
+          <Link to="/dashboard/keywords">
+            {" "}
+            <i className="fa fa-hashtag"></i>Keywords{" "}
+          </Link>
+        </li>
+        <li className={activeCheck("/dashboard/sources")}>
+          <Link to="/dashboard/sources">
+            {" "}
+            <i className="fa fa-play-circle"></i>Sources{" "}
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
-export default Siderbar
+export default Siderbar;
